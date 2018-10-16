5bd91e5ded5dc43a3a51f6472f53fef35822236b
Hassan Sani <saniyhassan@gmail.com>
2018-10-16 01:17:36 +0100

    Set for hacktoberfest collection

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# african.js
-Inspired by my medium blog on https://medium.com/@inidaname
+# african Developers
+A curated list of African developers around the globe, 
+
+## How to contribute
+Let start
+
+#### First things first
+
+Check the [existing list](https://hassansani.me/african). For inspirations
+
+#### Next
+If you are not already on this list, this is how you can add it
+
+* `fork` this `repo`
+* `clone` it to you local machine
+* open the [data]() file
+* Add yours in the format below
+* Send a `pull request` 🎉
+
+```
+  {
+    "name": "Hassan Sani",
+    "github": "inidaname",
+    "resident": "Nigeria",
+    "country": "Nigeria",
+    "gender": "Male",
+    "stack": "JavaScript, Typescript, Node.js, Angular, HTML, CSS, C++",
+    "twitter": "inidaname",
+    "url": "hassansani.me",
+    "profession": "Fullstack Developer"
+  },
+```
+
+**Name**: Name of the podcast,<br/>
+**GitHub**: GitHub profile name<br/>
+**URL**: Your Web address if Any<br/>
+**Profession**: What is the title of your Profession.<br/>
+**Stack**: The dev stack you are most comfortable with.<br/>
+**Twitter**: Your twitter Name.<br/>
+**Gender**: This really have no importance, but feel free to add your<br/>
+**Resident**: Your Country of residence<br/>
+**Country**: Your Country of Origin
+
+
+## Contributing
+
+Thank you for taking the time to contribute! 👍🎉
+
+Follow the steps above and you are done.
+
+
+## Authors
+
+* **Hassan Sani** - *Initial work* - [iNidAName](https://github.com/iNidAName)
+* **Ahmad Abdullaziz** - Docs - [dev_amaz](https://github.com/dev_amaz)
+
+See also the list of [contributors](https://github.com/inidaname/africandevs/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ index.js
@@ -0,0 +1,91 @@
+"use strict";
+var broadcast;
+var frequency;
+var myRequest = new Request('./data/people.json');
+var categories = new Promise(function (resolve, reject) {
+    fetch(myRequest)
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (response) {
+        var newCategory = [];
+        var finalCategory = [];
+        response.map(function (obj) {
+            var cat = obj.stack.split(',').map(function (v) { return v.trim().toLowerCase(); });
+            newCategory.push.apply(newCategory, cat);
+        });
+        newCategory.map(function (v) {
+            if (v.length !== 0 && finalCategory.indexOf(v) < 0) {
+                finalCategory.push(v);
+            }
+        });
+        var podCastList = [finalCategory];
+        if (podCastList) {
+            return resolve({ podCastList: podCastList, response: response });
+        }
+        else {
+            return reject('Empty Pod cast');
+        }
+    });
+});
+categories.then(function (res) {
+    var re = res.podCastList;
+    var category = document.createElement('ul');
+    re[0].map(function (v, i) {
+        var theChildren = document.createElement('li');
+        theChildren.appendChild(document.createTextNode(v.toUpperCase()));
+        theChildren.setAttribute('class', 'subMenu');
+        theChildren.addEventListener('click', function (event) {
+            console.log(event);
+        });
+        return category.appendChild(theChildren);
+    }).join(' ');
+    var cat = document.getElementById('category');
+    cat.appendChild(category);
+    var contents = res.response;
+    var podcastEl = document.createElement('ul');
+    podcastEl.classList.add('podcast');
+    contents.map(function (v, i) {
+        console.log(v);
+        var mainURL = (v.url !== undefined && v.url.substring(0, 4) !== 'http') ? "http://" + v.url : v.url;
+        var listBe = "\n        <a href=\"" + mainURL + "\" target=\"_blank\">\n            <li class=\"podList\">\n                <div>\n                    <h3> " + v.name + " </h3>\n                    <img src=\"" + v.logo + "\" alt=\"" + v.name + "\">\n                    <ul>\n                        <li>\n                            <strong>Country of Resident:</strong> <br> " + v.country + "\n                        </li>\n                        <li>\n                            <strong>Origin Country:</strong> <br> " + v.resident + "\n                        </li>\n                        <li>\n                            <span><strong>Gender:</strong>" + v.gender + "</span>\n                        </li>\n                        <li>\n                            <strong>Stack:</strong> <br> " + v.stack + "\n                        </li>\n                        <li>\n                            <strong>Profession:</strong> <br> " + v.profession + "\n                        </li>\n                    </ul>\n                </div>\n            </li>\n        </a>\n        ";
+        return podcastEl.append(document.createRange().createContextualFragment(listBe));
+    }).join(' ');
+    var mainList = document.getElementById('mainList');
+    mainList.appendChild(podcastEl);
+});
+var listMenu = document.querySelectorAll('#sideContent li');
+var nullMe;
+var listTrig;
+listMenu.forEach(function (el) {
+    el.addEventListener('click', function (ev) {
+        listMenu.forEach(function (e) {
+            if (e.getAttribute('id') !== ev.target.attributes.getNamedItem('id').value) {
+                e.style.opacity = '0';
+                e.children[0].style.opacity = '0';
+                e.children[0].style.height = '0px';
+                e.children[0].style.overFlow = 'none';
+                e.classList.remove('checkMe');
+            }
+            else {
+                if (e.classList.contains('checkMe')) {
+                    e.classList.remove('checkMe');
+                    el.style.opacity = '1';
+                    e.children[0].style.opacity = '0';
+                    e.children[0].style.overFlow = 'none';
+                    e.children[0].style.height = '0px';
+                }
+                else {
+                    e.classList.add('checkMe');
+                    e.style.opacity = '1';
+                    e.children[0].style.opacity = '1';
+                    e.children[0].style.height = '400px';
+                    e.children[0].style.overFlow = 'block';
+                }
+            }
+        });
+    });
+});
+function openNav() {
+    console.log();
+}
